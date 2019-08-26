13a0e7b5aa21374a68064857e637696d62d796df
schneems <richard.schneeman+foo@gmail.com>
2019-08-26 11:08:31 -0500

    Logging

--- client/script.rb
+++ client/script.rb
@@ -1,9 +1,14 @@
 require 'monitor'
 require 'excon'
 
+require 'date'
+require 'pathname'
+require 'fileutils'
+LOG_DIR = Pathname.new(__FILE__).join("../logs/clients/#{Time.now.strftime('%Y-%m-%d-%H-%M-%s-%N')}")
+FileUtils.mkdir_p(LOG_DIR)
+
 CLIENT_COUNT = ENV.fetch("CLIENT_COUNT") { 5 }.to_i
 PROCESS_COUNT = ENV.fetch("PROCESS_COUNT") { 2 }.to_i
-
 module RateLimit
   MAX_LIMIT = 4500.to_f
   @monitor = Monitor.new # Reentrant mutex
@@ -18,6 +23,12 @@ module RateLimit
     sleep_for = @client_guess * 1/(@arrival_rate / 3600)
     jitter = sleep_for * rand(0.0..0.1)
     return sleep_for + jitter
+  end
+
+  def self.log(req)
+    @monitor.synchronize do
+      File.open(LOG_FILE, 'a') { |f| f.puts("#{DateTime.now.iso8601},#{@sleep_for.to_s}") }
+    end
   end
 
   def self.call(&block)
@@ -37,6 +48,7 @@ module RateLimit
     status_string << "#remaining=#{remaining} "
     status_string << "#sleep_for=#{sleep_for} "
     puts status_string
+    log(req)
 
     @monitor.synchronize do
       if req.status == 429
@@ -92,15 +104,12 @@ def spawn_threads
   threads.map(&:join)
 end
 
-def spawn_processes
-  PROCESS_COUNT.times.each do
-    fork do
-      spawn_threads
-    end
+PROCESS_COUNT.times.each do
+  fork do
+    LOG_FILE = LOG_DIR.join(Process.pid.to_s).to_s
+    spawn_threads
   end
-
-  Process.waitall
 end
 
-spawn_processes
+Process.waitall
 
