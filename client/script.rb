50e1dd7d0ead884e9d540597ee5036cda9c805bf
schneems <richard.schneeman+foo@gmail.com>
2019-08-26 11:02:51 -0500

    Using sleep

--- client/script.rb
+++ client/script.rb
@@ -1,30 +1,32 @@
 require 'monitor'
 require 'excon'
 
+require 'date'
+require 'pathname'
+require 'fileutils'
+LOG_DIR = Pathname.new(__FILE__).join("../logs/clients/#{Time.now.strftime('%Y-%m-%d-%H-%M-%s-%N')}")
+FileUtils.mkdir_p(LOG_DIR)
+
 CLIENT_COUNT = ENV.fetch("CLIENT_COUNT") { 5 }.to_i
 PROCESS_COUNT = ENV.fetch("PROCESS_COUNT") { 2 }.to_i
-
 module RateLimit
   MAX_LIMIT = 4500.to_f
+  MIN_SLEEP = 1/(MAX_LIMIT / 3600)
   @monitor = Monitor.new # Reentrant mutex
-  @arrival_rate = MAX_LIMIT
-  @client_guess = nil
-  @rate_limit = 0
+  @sleep_for = 0.0
+  @rate_limit_count = 0
 
-  def self.sleep_for_client_count
-    return 0 if @client_guess.nil?
-    return 0 if @client_guess < 0
-
-    sleep_for = @client_guess * 1/(@arrival_rate / 3600)
-    jitter = sleep_for * rand(0.0..0.1)
-    return sleep_for + jitter
+  def self.log(req)
+    @monitor.synchronize do
+      File.open(LOG_FILE, 'a') { |f| f.puts("#{DateTime.now.iso8601},#{@sleep_for.to_s}") }
+    end
   end
 
   def self.call(&block)
-    rate_limit_count = @rate_limit
+    rate_limit_count = @rate_limit_count
 
-    sleep_for = sleep_for_client_count
-    sleep(sleep_for)
+    jitter = @sleep_for * rand(0.0..0.1)
+    sleep(@sleep_for + jitter)
 
     req = yield
 
@@ -33,10 +35,10 @@ module RateLimit
     status_string = String.new("")
     status_string << "#{Process.pid}##{Thread.current.object_id}: "
     status_string << "#status=#{req.status} "
-    status_string << "#client_guess=#{@client_guess || 0} "
     status_string << "#remaining=#{remaining} "
-    status_string << "#sleep_for=#{sleep_for} "
+    status_string << "#sleep_for=#{@sleep_for} "
     puts status_string
+    log(req)
 
     @monitor.synchronize do
       if req.status == 429
@@ -50,13 +52,10 @@ module RateLimit
         # If this value is different than the value recorded at the beginning of the
         # request then it means another thread has already increased the client guess
         # and we should try using that value first before we try bumping it.
-        if rate_limit_count == @rate_limit
-          @client_guess ||= 2
-          @client_guess *= 2
-          @rate_limit += 1
-
-          multiplier = req.headers["RateLimit-Multiplier"].to_f
-          @arrival_rate = multiplier * MAX_LIMIT
+        if rate_limit_count == @rate_limit_count
+          @sleep_for += MIN_SLEEP if @sleep_for <= 0
+          @sleep_for *= 2
+          @rate_limit_count += 1
         end
 
         # Retry the request with the new sleep value
@@ -65,7 +64,8 @@ module RateLimit
         # The fewer available requests, the slower we should reduce our client guess.
         # We want this to converge and linger around a correct value rather than
         # being a true sawtooth pattern.
-        @client_guess -= remaining / MAX_LIMIT if @client_guess
+        @sleep_for -= remaining/MAX_LIMIT * MIN_SLEEP
+        @sleep_for = 0 if @sleep_for <= 0
       end
     end
 
@@ -92,15 +92,12 @@ def spawn_threads
   threads.map(&:join)
 end
 
-def spawn_processes
-  PROCESS_COUNT.times.each do
-    fork do
-      spawn_threads
-    end
+PROCESS_COUNT.times.each do
+  fork do
+    LOG_FILE = LOG_DIR.join(Process.pid.to_s).to_s
+    spawn_threads
   end
-
-  Process.waitall
 end
 
-spawn_processes
+Process.waitall
 
--- server/config.ru
+++ server/config.ru
@@ -1,5 +1,13 @@
 require 'thread'
 
+require 'pathname'
+require 'fileutils'
+require 'date'
+LOG_DIR = Pathname.new(__FILE__).join("../logs/server/#{Time.now.strftime('%Y-%m-%d-%H-%M-%s-%N')}")
+FileUtils.mkdir_p(LOG_DIR)
+LOG_FILE = LOG_DIR.join(Process.pid.to_s)
+@rate_limit_count = 0
+
 MAX_REQUESTS = 4500
 limit_left = MAX_REQUESTS.to_f
 last_request = Time.now
@@ -7,6 +15,7 @@ rate_of_limit_gain = limit_left / 3600
 mutex = Mutex.new
 
 app = -> (env) do
+  remaining = nil
   mutex.synchronize do
     limit_left -= 1 if limit_left > 0
 
@@ -14,12 +23,14 @@ app = -> (env) do
       current_request = Time.now
       time_diff = current_request - last_request
       last_request = current_request
-      puts "current: #{current_request}, last: #{last_request} Time Left: #{limit_left} diff between requests: #{time_diff} addition: #{limit_left + time_diff * rate_of_limit_gain}"
       limit_left = [limit_left + time_diff * rate_of_limit_gain, MAX_REQUESTS].min
     end
+
+    remaining = [limit_left.floor, 0].max
+    @rate_limit_count += 1 if remaining <= 0
+    File.open(LOG_FILE, 'a') { |f| f.puts("#{DateTime.now.iso8601},#{@rate_limit_count.to_s}") }
   end
 
-  remaining = [limit_left.floor, 0].max
   headers = { "RateLimit-Remaining" => remaining, "RateLimit-Multiplier" => 1, "Content-Type" => "text/plain" }
   if remaining <= 0
     status = 429
@@ -29,7 +40,6 @@ app = -> (env) do
     body = "<3<3<3 Hello world <3<3<3"
   end
 
-  puts headers.inspect
   return [status, headers, [body]]
 end
 
