9b021717c27812ebd573fd141777aaf0f9e34668
schneems <richard.schneeman+foo@gmail.com>
2019-08-27 16:16:46 -0500

    Fix and extra logging

--- client/script.rb
+++ client/script.rb
@@ -14,7 +14,7 @@ module RateLimit
   MIN_SLEEP = 1/(MAX_LIMIT / 3600)
   @monitor = Monitor.new # Reentrant mutex
   @sleep_for = 2 * MIN_SLEEP
-  @rate_limit_count = 0
+  @rate_limit_count = 1
   @times_retried = 0
   @retry_thread = nil
 
@@ -42,28 +42,29 @@ module RateLimit
     @monitor.synchronize do
       remaining = req.headers["RateLimit-Remaining"].to_i
 
-      @sleep_for -= (remaining/(10*MAX_LIMIT/@sleep_for))
+      @sleep_for -= (remaining/(100*MAX_LIMIT/@sleep_for))
       @sleep_for = MIN_SLEEP if @sleep_for < 0
     end
   end
 
   def self.retry_request_logic(&block)
-    i_am_retrying = false
     @monitor.synchronize do
-      if @retry_thread_id.nil? || @retry_thread == Thread.current
+      if @retry_thread.nil? || @retry_thread == Thread.current
         # First retry request, only increase sleep value if retry doesn't work.
         # Should guard against run-away high sleep values
-        @sleep_for *= 2 if @times_retried != 0
+        if @times_retried != 0
+          @sleep_for *= 2
+          @rate_limit_count += 1
+        end
 
         @times_retried += 1
         @retry_thread = Thread.current
-        i_am_retrying = true
       end
     end
 
     # Retry the request with the new sleep value
     req = self.call(&block)
-    if i_am_retrying
+    if @retry_thread == Thread.current
       @monitor.synchronize do
         @times_retried = 0
         @retry_thread = nil
@@ -86,6 +87,7 @@ module RateLimit
     status_string << "#{Process.pid}##{Thread.current.object_id}: "
     status_string << "#status=#{req.status} "
     status_string << "#remaining=#{remaining} "
+    status_string << "#rate_limit_count=#{@rate_limit_count} "
     status_string << "#sleep_for=#{@sleep_for} "
     puts status_string
   end
