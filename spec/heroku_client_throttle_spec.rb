e4e1b92ace1dbf6d1a1cf8524e9f7ed0d960a057
schneems <richard.schneeman+foo@gmail.com>
2019-09-04 15:25:23 -0500

    Test rate limit call logic

--- spec/heroku_client_throttle_spec.rb
+++ spec/heroku_client_throttle_spec.rb
@@ -8,7 +8,9 @@ describe 'Heroku client throttle' do
     sleep_start = client.sleep_for
     multiply_at_start = client.rate_limit_multiply_at
 
+    @times_called = 0
     client.call do
+      @times_called += 1
       if client.rate_limit_count < 1
         FakeResponse.new(429)
       else
@@ -19,6 +21,7 @@ describe 'Heroku client throttle' do
     sleep_end = client.sleep_for
     multiply_at_end = client.rate_limit_multiply_at
 
+    expect(@times_called).to eq(3) # Once for initial 429, once for second 429, once for final 200
     expect(sleep_end).to be_between(sleep_start, sleep_start * 2.1)
     expect(multiply_at_end).to_not eq(multiply_at_start)
     expect(multiply_at_end).to be_between(multiply_at_start, Time.now)
