1ed9ebbb155e63bf315e6471ba9258ff6e114e40
schneems <richard.schneeman+foo@gmail.com>
2019-09-08 07:41:02 +0700

    Update chart script

--- chart.rb
+++ chart.rb
@@ -8,8 +8,10 @@ client_log_dir = Pathname.new(client_log_dir)
 
 g = Gruff::Line.new
 
-log_entry = client_log_dir.entries.sort_by do |entry|
-  client_log_dir.join(entry).mtime
+log_entry = client_log_dir.entries.map do |entry|
+  client_log_dir.join(entry)
+end.select { |dir| dir.directory? && dir != client_log_dir }.sort_by do |dir|
+  dir.mtime
 end.last
 
 log_dir = client_log_dir.join(log_entry)
@@ -22,13 +24,38 @@ raise "#{log_dir} is not a directory" unless log_dir.directory?
 # puts log_dir
 STDOUT.sync=true
 
-log_dir.each_entry do |entry|
-  file = log_dir.join(entry)
-  next unless file.file?
-  next if entry.to_s == "chart.png"
-
-  g.data entry, file.each_line.map(&:to_f)
+process_count = 0
+entry_count = 0
+log_files = log_dir.entries.map do |entry|
+  log_dir.join(entry)
+end.select do |file|
+  next if file.basename.to_s == "chart.png"
+  next if !file.file?
+  true
 end
+
+log_files.each do |entry|
+  g.data entry.basename, entry.each_line.map(&:to_f)
+end
+
+
+entry_count = log_files.first.each_line.count
+
+time_scale = ENV.fetch("TIME_SCALE") { 1 }.to_i
+
+g.title = "API Client Rate Limit Throttling Sleep Values\nOver Time for #{log_files.count} PIDs"
+g.y_axis_label = "Sleep time in seconds"
+g.x_axis_label = "Time duration in hours"
+
+label_hash = { 0 => '0', entry_count - 1 => ((entry_count * time_scale) / 3600.0).to_s }
+hours = (entry_count * time_scale) / 3600.0
+hour_distance = (entry_count / hours.floor.to_f).floor
+hours.floor.times.each do |hour|
+  hour += 1
+  label_hash[hour_distance * hour] = hour.to_s
+end
+
+g.labels = label_hash
 
 # lines = File.open("/Users/rschneeman/Documents/projects/ratelimit-demo/logs/clients/2019-09-04-17-36-1567636587-254648000/19591").each_line
 g.write(log_dir.join('chart.png'))
